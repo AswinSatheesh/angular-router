74ed4d5f6582d4789699b279350a09b06c15266f
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2024-10-27 11:41:10 +0530

    fragment route

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls:['./home.component.css']
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit{
+    activeRoute : ActivatedRoute = inject(ActivatedRoute);
+    ngOnInit(){
+        this.activeRoute.fragment.subscribe((data)=>{
+            //  console.log(data);
+            this.JumpToSection(data);
+             
+        })
+    }
 
+    JumpToSection(section : any){
+        document.getElementById(section)?.scrollIntoView({behavior: 'smooth'})
+    }
 }
